d7c38d4161fdb0de2e3ca187a725b376874f5aea
Mikhail <shaltynovm@gmail.com>
2023-07-07 00:58:37 +0530

    initial commit

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
+
+
+
     public void calculateSum(int number) {
+        int first = number / 1000;
+        int second =(number%1000)/100;
+        int third=(number%100)/10;
+        int forth =number%10;
+        int finalSum = first + second + third + forth;
+        System.out.println(finalSum);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+
+
     public void revert(int number) {
+        int first = number / 100;
+        int second = (number%100)/10;
+        int third = number % 10;
+        int finalDigit = third*100+second*10+first;
+        System.out.println(finalDigit);
     }
 }
